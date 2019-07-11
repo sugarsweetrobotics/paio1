555d268b0235859fb3bb8bba6d67ca91471fadab
Yuki Suga <ysuga@ysuga.net>
2019-07-12 08:20:22 +0900

    update process dictionary

--- include/paio/proc/process.h
+++ include/paio/proc/process.h
@@ -7,18 +7,26 @@ namespace paio
 {
 
 class Process {
+
 public:
-    const std::string name;
-    const std::shared_ptr<IProcessFunction> fp;
+    static Process Null;
 public:
-    
+    std::string name;
+    std::shared_ptr<IProcessFunction> fp;
+public:
+    Process(): name("null"), fp(nullptr) {}
     Process(const Process& p) : name(p.name), fp(p.fp) {}
     Process(Process&& p) : name(std::move(p.name)), fp(std::move(p.fp)) {}
     explicit Process(const std::string& n, IProcessFunction* func): name(n), fp(func) {}
     virtual ~Process() {}
 
 public:
-    
+    Process& operator=(const Process& p) {
+        this->name = p.name;
+        this->fp = p.fp;
+        return *this;
+    }
+    friend bool isNull(const paio::Process& proc);
 };
 
 template<typename T, typename... R>
@@ -30,5 +38,9 @@ Process process(const std::string& name, T(*f)(R...)) {
 template<typename T, typename...R>
 inline std::optional<T> call(const Process& p, R... args) {
    return paio::call<T, R...>(std::dynamic_pointer_cast<ProcessFunction<T, R...>>(p.fp).get(), args...);
+}
+
+inline bool isNull(const paio::Process& proc) {
+    return !(proc.fp);
 }
 } // namespace paio
---
+++ include/paio/proc/process_dictionary.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include "paio/proc/process.h"
+
+namespace paio
+{
+
+
+using OnPutProcess = std::function<paio::Process(paio::Process&&)>;
+using OnPutProcessMap = std::map<std::string, OnPutProcess>;
+using OnPutProcessMap_ptr = std::shared_ptr<OnPutProcessMap>;
+
+
+class ProcessDictionary;
+using ProcessDictionary_ptr = std::shared_ptr<ProcessDictionary>;
+
+struct ProcessDictionary
+{
+private:
+  OnPutProcessMap_ptr onPutProcesses;
+public:
+  std::map<std::string, Process> map;
+
+public:
+  ProcessDictionary(): onPutProcesses(std::make_shared<OnPutProcessMap>()), map() {}
+
+  //friend Process registerOnPutProcess(Process_ptr dic, const std::string& key, OnPutProcess onPut);
+
+  friend void put(ProcessDictionary_ptr dic, const std::string &key, paio::Process &&oc);
+  friend void put(ProcessDictionary_ptr dic, const std::string &key, const paio::Process &oc);
+  friend Process& get(ProcessDictionary_ptr dic, const std::string& key);
+};
+
+ProcessDictionary_ptr process_dictionary() {
+    return std::make_shared<ProcessDictionary>();
+}
+
+void put(ProcessDictionary_ptr dic, const std::string &key, const paio::Process &proc)
+{
+    dic->map[key] = proc;
+}
+
+void put(ProcessDictionary_ptr dic, const std::string &key, paio::Process &&proc)
+{
+    dic->map[key] = std::move(proc);
+}
+
+Process& get(ProcessDictionary_ptr dic, const std::string &key)
+{
+  if (dic->map.count(key) == 0)
+  {
+    return paio::Process::Null;
+  }
+  return dic->map[key];
+}
+
+Process paio::Process::Null;
+/*
+
+
+template<typename T>
+T getContainer(ObjectDictionary_ptr dic, const std::string& key, std::function<T(const paio::ObjectContainer&)> f) {
+  if (dic->map.count(key) == 0)
+  {
+    return f(paio::ObjectContainer());
+  }
+  return f(dic->map[key]);
+}
+*/
+
+/*
+template <typename T>
+void put(ObjectDictionary_ptr dic, const std::string &key, const paio::Object<T> &doc)
+{
+  put(dic, key, ObjectContainer(*doc.get()));
+}
+
+void put(std::shared_ptr<ObjectDictionary> dic, const std::string &key, paio::ObjectContainer &&oc);
+
+
+inline void forEach(ObjectDictionary_ptr dic, void (*f)(const std::string &, paio::ObjectContainer &))
+{
+  for (auto &[key, value] : dic->map)
+  {
+    f(key, value);
+  }
+}
+
+inline void forEach(ObjectDictionary_ptr dic, std::function<void(const std::string &, paio::ObjectContainer &)> f)
+{
+  for (auto &[key, value] : dic->map)
+  {
+    f(key, value);
+  }
+}
+
+inline ObjectDictionary_ptr registerOnPutObjectContainer(ObjectDictionary_ptr dic, const std::string& key, OnPutObjectContainer onPut) {
+  (*(dic->onPutObjectContainers))[key] = onPut;
+  return dic;
+}
+*/
+}; // namespace paio
